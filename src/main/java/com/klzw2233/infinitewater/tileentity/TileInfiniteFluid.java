2874989d40a9a9961b791f0e4637303658013a50
klzw2233 <jimmyliubus2020@gmail.com>
2025-09-01 08:31:43 +0800

    try2 : fix the updateEntity method.

--- src/main/java/com/klzw2233/infinitewater/tileentity/TileInfiniteFluid.java
+++ src/main/java/com/klzw2233/infinitewater/tileentity/TileInfiniteFluid.java
@@ -27,7 +27,7 @@ public class TileInfiniteFluid extends TileBase implements IFluidHandler{
     @Override
     public void updateEntity() {
         // 只在服务器端执行
-        if (worldObj.isRemote) return;
+        // if (worldObj.isRemote) return;
 
         // 每 5 tick 执行一次（减少性能压力）
         // if (worldObj.getTotalWorldTime() % 5 != 0) return;
